d312cb51fd7476c54b142c2fa822a860b4e3ee43
0x1317bf7 <823222063@qq.com>
2021-05-22 02:14:57 +0800

    Create renamer.py
    批量修改文件后缀名

---
+++ renamer.py
@@ -0,0 +1,26 @@
+#批量修改文件后缀名
+
+import os
+
+def rename(path, old, new, rec = False):
+    print("进入" + path)
+    for file in os.listdir(path):
+        p = os.path.join(path, file)
+        if os.path.isfile(p):
+            if file.endswith(old):
+                print("将" + file + "重命名")
+                os.rename(p, p[0:-len(old)] + new)
+        elif rec and os.path.isdir(p):
+            rename(p)
+
+def main():
+    path = input("请输入文件夹路径")
+    rec = True if input("是否递归?(Y/N)").lower() == "y" else False
+
+    old = input("需要更改的后缀名?")
+    new = input("更改为?")
+
+    rename(path, old, new, rec)
+
+if __name__ == "__main__":
+    main()
